644124f3d0b7047b0769f2456f9b89c8aaf9785d
Miro Svrtan <miro@mirosvrtan.me>
2019-12-25 21:29:23 +0100

    Structs should product private properties, constructor and getters

--- lib/PhpParser/Node/Stmt/Struct_.php
+++ lib/PhpParser/Node/Stmt/Struct_.php
@@ -19,12 +19,82 @@ class Struct_ extends Class_
 		$this->implements = [];
 		$this->stmts = [];
 
+		$properties = [];
+		$methods = [];
+
 		/** @var Node\Param $property */
 		foreach ($parameters as $parameter) {
 			$props = [new PropertyProperty($parameter->var->name, null)];
-			$attributes = ['comments' => [new Comment('/** @var '.$parameter->type->name.' */')]];
-			$this->stmts[] = new Property(Class_::MODIFIER_PUBLIC, $props, $attributes);
+			if($parameter->type instanceof Node\Name){
+				$type=$parameter->type->toCodeString();
+			}elseif($parameter->type instanceof Node\Identifier){
+				$type=$parameter->type->name;
+			}elseif($parameter->type instanceof Node\NullableType){
+				$type=$parameter->type->type->toString().'|null';
+			}
+
+			$attributes = ['comments' => [new Comment('/** @var '.$type.' */')]];
+			$properties[] = new Property(Class_::MODIFIER_PRIVATE, $props, $attributes);
 		}
+
+		$constructorStmts = [];
+
+		if (count($parameters) > 0) {
+
+			/** @var Node\Param $property */
+			foreach ($parameters as $parameter) {
+				$constructorStmts[] =
+					new Expression(
+						new Node\Expr\Assign(
+							new Node\Expr\PropertyFetch(
+								new Node\Expr\Variable('this'),
+								$parameter->var->name
+							),
+							new Node\Expr\Variable($parameter->var->name)
+						)
+					);
+			}
+
+			$methods[] = new ClassMethod(
+				'__construct',
+				[
+					'type'   => Class_::MODIFIER_PUBLIC,
+					'params' => $parameters,
+					'stmts'  => $constructorStmts
+				]
+			);
+		}
+
+		/** @var Node\Param $property */
+		foreach ($parameters as $parameter) {
+			$stmts = [
+				new Return_(
+					new Node\Expr\PropertyFetch(
+						new Node\Expr\Variable('this'),
+						$parameter->var->name
+					)
+				)
+			];
+
+
+			if($parameter->type instanceof Node\Name){
+				$returnType=$parameter->type->toCodeString();
+			}elseif($parameter->type instanceof Node\Identifier){
+				$returnType=$parameter->type->name;
+			}elseif($parameter->type instanceof Node\NullableType){
+				$returnType='?'.$parameter->type->type->toString();
+			}
+
+			$methods[] = new ClassMethod(
+				'get'.ucfirst($parameter->var->name),
+				[
+					'type'       => Class_::MODIFIER_PUBLIC,
+					'returnType' => $returnType,
+					'stmts'      => $stmts
+				]
+			);
+		}
+		$this->stmts = array_merge($properties, $methods);
 	}
 
 	public function getType(): string
